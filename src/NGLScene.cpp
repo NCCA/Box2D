c126f32d5b821de9bf4062a0bdd6cd18802085da
jmacey <jmacey@bournemouth.ac.uk>
2015-11-03 16:24:43 +0000

    updated to work for Qt5.5.1 and Qt 5.5.0

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -31,6 +31,12 @@ void NGLScene::resizeGL(QResizeEvent *_event)
   m_height=_event->size().height()*devicePixelRatio();
 }
 
+void NGLScene::resizeGL(int _w , int _h)
+{
+  m_width=_w*devicePixelRatio();
+  m_height=_h*devicePixelRatio();
+}
+
 void NGLScene::createStaticBodies()
 {
   float x=-30.0f;
