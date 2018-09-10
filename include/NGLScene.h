9bb2ea813c3abc0bf4b934c8843ca77468ff41c9
jmacey <jmacey@bournemouth.ac.uk>
2018-09-10 11:37:18 +0100

    updated for NGL7

--- include/NGLScene.h
+++ include/NGLScene.h
@@ -1,13 +1,10 @@
 #ifndef NGLSCENE_H_
 #define NGLSCENE_H_
-#include <ngl/Camera.h>
-#include <ngl/Colour.h>
-#include <ngl/Light.h>
+#include <QOpenGLWindow>
+#include <QSet>
 #include <ngl/Transformation.h>
 #include <ngl/Text.h>
 #include <Box2D/Box2D.h>
-#include <QOpenGLWindow>
-#include <QSet>
 #include <memory>
 //----------------------------------------------------------------------------------------------------------------------
 /// @file NGLScene.h
@@ -33,7 +30,7 @@ class NGLScene : public QOpenGLWindow
     //----------------------------------------------------------------------------------------------------------------------
     /// @brief dtor must close down ngl and release OpenGL resources
     //----------------------------------------------------------------------------------------------------------------------
-    ~NGLScene();
+    ~NGLScene() override;
     //----------------------------------------------------------------------------------------------------------------------
     /// @brief the initialize class is called once when the window is created and we have a valid GL context
     /// use this to setup any default GL stuff
